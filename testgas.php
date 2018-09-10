84feeedcf44f4af0c49b66ebb4ff5ab166a40c73
jhidalgo-macpro <jhidalgo@um.es>
2018-09-10 10:46:51 +0200

    Additional example considering the last 5 blocks

--- testgas.php
+++ testgas.php
@@ -12,12 +12,13 @@ if (isset($options['b'])) $blocks = $options['b'];
 if (isset($options['u'])) $url = $options['u'];
 
 echo "URL " . $url.PHP_EOL;
-echo "Study based on ".$blocks. " blocks".PHP_EOL;
-echo "Medium/cheap gasPrice, according to the last ".$blocks." blocks = ". getGasPriceEstimation($url, $blocks, $speed = "medium") . ' gwei'.PHP_EOL;
+echo "Medium/cheap gasPrice, according to the last 5 (example) blocks = ". getGasPriceEstimation($url, 5, $speed = "medium") . ' gwei'.PHP_EOL;
 
 
 //To browse full returned jsons
+echo "Study based on ".$blocks. " blocks".PHP_EOL;
 $full = analyzeGasPrice($url, $blocks);
 echo "Slow/cheapest gasPrice, according to the last ".$blocks." blocks = ". $full['percentile_10'] . ' gwei'.PHP_EOL;
+echo "Medium/cheap gasPrice, according to the last ".$blocks." blocks = ". $full['percentile_25'] . ' gwei'.PHP_EOL;
 echo "Fast/expensive gasPrice, according to the last ".$blocks." blocks = ". $full['percentile_50']. ' gwei'.PHP_EOL;
 
